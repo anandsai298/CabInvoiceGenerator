8c0cf0ea0d9a2737419a91256fa9cdf34f4ef83d
anandsaikumar <anandsai298@gmail.com>
2023-04-13 18:59:56 +0530

    [sai] Add : calculation of fare and if fare is lessthan min fare take min fare

---
+++ CabInvoiceGenerator/CabInvoiceGenerator/InvoiceGenerator.cs
@@ -0,0 +1,15 @@
+﻿namespace CabInvoiceGeneratorApp
+{
+    public class InvoiceGenerator
+    {
+        private static readonly double COST_PER_KM = 10.0;
+        private static readonly double COST_PER_MIN = 1.0;
+        private static readonly double MINIMUM_FARE = 5.0;
+        private double CabFare = 0.0;
+        public double CalculateFare(double Distance,double Time)
+        {
+            this.CabFare = (Distance* COST_PER_KM)+(Time* COST_PER_MIN);
+            return Math.Max(this.CabFare, MINIMUM_FARE);
+        }
+    }
+}
